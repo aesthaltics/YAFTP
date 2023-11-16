26eb488a6aaa5f86a157d618e921d19ef558ad37
aesthaltics <aesthaltics@pm.me>
2023-11-16 14:05:23 +0100

    Add new database and user types to environment.d.ts

--- environment.d.ts
+++ environment.d.ts
@@ -5,8 +5,23 @@ declare global {
 			DATABASE_HOST: string;
 			DATABASE_PORT: number;
 			DATABASE_NAME: string;
+
+			DATABASE_TABLES: { [table_name: string]: string };
+			DATABASE_FIELDS: {
+				[table_name: string]: {
+					[columnName: string]: string;
+				};
+			};
+			SCRYPT_N: number;
+			SCRYPT_r: number;
+			SCRYPT_p: number;
+			SCRYPT_dkLen: number;
 		}
 	}
+	type User = {
+		userName: string;
+		password: string;
+	};
 }
 
 export {};
