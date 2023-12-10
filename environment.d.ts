8362ff782b225462de8b331f2dd5f341f2af05dd
aesthaltics <aesthaltics@pm.me>
2023-12-10 23:52:33 +0100

    Update environment variables and types

--- environment.d.ts
+++ environment.d.ts
@@ -5,10 +5,10 @@ import { URL } from "url";
 declare global {
 	namespace NodeJS {
 		interface ProcessEnv {
-			DATABASE_USER: string;
-			DATABASE_HOST: string;
-			DATABASE_PORT: number;
-			DATABASE_NAME: string;
+			PGUSER: string;
+			PGHOST: string;
+			PGPORT: number;
+			PGDATABASE: string;
 			HTTPS_KEY_PASSPHRASE: string;
 		}
 	}
@@ -17,17 +17,42 @@ declare global {
 		password: string;
 	};
 
-	type RequestContext= {
-		requestId: string
+	type RequestContext = {
+		requestId: string;
 		userId?: string;
 		isAuthenticated: boolean;
 		request: IncomingMessage;
 		response: ServerResponse;
 		URL: URL;
 		isAuthorized: boolean;
-		stream?: Readable
-		log?: (logText: string) => void
+		stream?: Readable;
+		log?: Logger
 	};
+
+	type Token = {
+		expiry: Date;
+		username: string;
+		attributes: { [attribute: string]: string };
+	};
+
+	type TokenStore = {
+		async create(token: Token): Promise<string>;
+		async read(tokenID: string): Promise<Token|undefined>;
+	};
+	type LOG_LEVEL =
+		| "DEBUG"
+		| "INFO"
+		| "NOTICE"
+		| "WARNING"
+		| "ERROR"
+		| "CRITICAL"
+		| "ALERT"
+		| "EMERGENCY";
+
+	type Logger = {
+		(logText: string,
+		logLevel?: LOG_LEVEL): void
+	}
 }
 
 export {};
