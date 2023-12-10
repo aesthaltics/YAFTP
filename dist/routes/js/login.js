f1ebf668eed5dfc2339e7cac602ac6b03763a4dc
aesthaltics <aesthaltics@pm.me>
2023-12-10 19:35:22 +0100

    Refactor login to use http authorization header

--- dist/routes/js/login.js
+++ dist/routes/js/login.js
@@ -10,27 +10,33 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 const LOGIN_USER_ROUTE = "/login-user";
 const requestLogin = (user) => __awaiter(void 0, void 0, void 0, function* () {
     var _a;
-    if (user.userName.length < 1 || user.password.length < 1) {
+    if (user.username.length < 1 || user.password.length < 1) {
         Promise.reject(new Error("not given username or password"));
     }
-    const stringifiedUser = JSON.stringify(user);
-    console.log(stringifiedUser);
+    // const stringifiedUser = JSON.stringify(user);
+    // console.log(stringifiedUser);
     const response = yield fetch(LOGIN_USER_ROUTE, {
         method: "POST",
-        body: stringifiedUser,
+        // body: stringifiedUser,
         headers: {
             "Content-Type": "text/javascript",
+            Authorization: `Basic ${btoa(`${user.username}:${user.password}`)}`,
         },
     });
     const loginResponse = yield response.json();
     const loginMessage = loginResponse.message;
+    console.log(loginMessage);
     const loginMessageParagraphId = "loginMessageParagraph";
     const loginForm = document.getElementById("login-form");
     const loginMessageParagraph = (_a = document.getElementById(loginMessageParagraphId)) !== null && _a !== void 0 ? _a : document.createElement("p");
-    if (!loginMessageParagraph.id) {
-        loginMessageParagraph.id = loginMessageParagraphId;
-        loginForm === null || loginForm === void 0 ? void 0 : loginForm.appendChild(loginMessageParagraph);
-    }
+    console.log(loginMessageParagraph);
+    loginMessageParagraph.id.length !== 0
+        ? loginMessageParagraph.id
+        : (() => {
+            console.log("running");
+            loginForm === null || loginForm === void 0 ? void 0 : loginForm.appendChild(loginMessageParagraph);
+            return loginMessageParagraphId;
+        })();
     if (loginMessage === "success") {
         loginMessageParagraph.innerText = "Success!";
         loginMessageParagraph.style["color"] = "green";
@@ -42,19 +48,16 @@ const requestLogin = (user) => __awaiter(void 0, void 0, void 0, function* () {
 });
 window.addEventListener("load", (event) => {
     const loginButton = document.getElementById("login-button");
-    loginButton === null || loginButton === void 0 ? void 0 : loginButton.addEventListener("click", (event) => {
+    const form = document.getElementsByTagName("form")[0];
+    form.addEventListener("submit", (event) => {
         event.preventDefault();
         const userNameElement = document.getElementById("user-name");
         const passwordElement = document.getElementById("password");
-        if (userNameElement instanceof HTMLInputElement &&
-            passwordElement instanceof HTMLInputElement) {
-            const userDetails = {
-                userName: userNameElement.value,
-                password: passwordElement.value,
-            };
-            console.log(userDetails);
-            requestLogin(userDetails);
-        }
+        const userDetails = {
+            username: userNameElement.value,
+            password: passwordElement.value,
+        };
+        requestLogin(userDetails);
     });
 });
 export {};
--- src/routes/js/login.ts
+++ src/routes/js/login.ts
@@ -4,27 +4,37 @@ const requestLogin = async (user: User) => {
 	if (user.username.length < 1 || user.password.length < 1) {
 		Promise.reject(new Error("not given username or password"));
 	}
-	const stringifiedUser = JSON.stringify(user);
-	console.log(stringifiedUser);
+	// const stringifiedUser = JSON.stringify(user);
+	// console.log(stringifiedUser);
 	const response = await fetch(LOGIN_USER_ROUTE, {
 		method: "POST",
-		body: stringifiedUser,
+		// body: stringifiedUser,
 		headers: {
 			"Content-Type": "text/javascript",
+			Authorization: `Basic ${btoa(`${user.username}:${user.password}`)}`,
 		},
 	});
 	const loginResponse = await response.json();
 	const loginMessage: string = loginResponse.message;
+
+	console.log(loginMessage);
 
 	const loginMessageParagraphId = "loginMessageParagraph";
 	const loginForm = document.getElementById("login-form");
 	const loginMessageParagraph =
 		document.getElementById(loginMessageParagraphId) ??
 		document.createElement("p");
-	if (!loginMessageParagraph.id) {
-		loginMessageParagraph.id = loginMessageParagraphId;
-		loginForm?.appendChild(loginMessageParagraph);
-	}
+
+	console.log(loginMessageParagraph);
+
+	loginMessageParagraph.id.length !== 0
+		? loginMessageParagraph.id
+		: ((): string => {
+				console.log("running");
+				loginForm?.appendChild(loginMessageParagraph);
+				return loginMessageParagraphId;
+		  })();
+
 	if (loginMessage === "success") {
 		loginMessageParagraph.innerText = "Success!";
 		loginMessageParagraph.style["color"] = "green";
@@ -35,23 +45,24 @@ const requestLogin = async (user: User) => {
 };
 
 window.addEventListener("load", (event) => {
-	const loginButton = document.getElementById("login-button");
-	loginButton?.addEventListener("click", (event): void => {
+	const loginButton = document.getElementById(
+		"login-button"
+	) as HTMLButtonElement;
+	const form = document.getElementsByTagName("form")[0];
+
+	form.addEventListener("submit", (event): void => {
 		event.preventDefault();
-		const userNameElement = document.getElementById("user-name");
-		const passwordElement = document.getElementById("password");
+		const userNameElement = document.getElementById(
+			"user-name"
+		) as HTMLInputElement;
+		const passwordElement = document.getElementById(
+			"password"
+		) as HTMLInputElement;
 
-		if (
-			userNameElement instanceof HTMLInputElement &&
-			passwordElement instanceof HTMLInputElement
-		) {
-			const userDetails: User = {
-				username: userNameElement.value,
-				password: passwordElement.value,
-			};
-			console.log(userDetails);
-
-			requestLogin(userDetails);
-		}
+		const userDetails: User = {
+			username: userNameElement.value,
+			password: passwordElement.value,
+		};
+		requestLogin(userDetails);
 	});
 });
